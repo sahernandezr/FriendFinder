ae59d4b5799ab31724a82acc63efaa8053929267
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2019-12-02 20:41:49 -0600

    added gitignore data

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friends = [
+    {
+        name: "Daria Morgendorffer",
+        photo: "https://vignette.wikia.nocookie.net/daria/images/d/de/Tumblr_lmpq9bwbyP1qj0esdo1_400.jpg/revision/latest/top-crop/width/200/height/150?cb=20110708181748",
+        scores: [
+            5,
+            5,
+            5,
+            1,
+            3,
+            3,
+            3,
+            4,
+            5,
+            1]
+    },
+    {
+        name: "Jane Lane",
+        photo: "https://vignette.wikia.nocookie.net/daria/images/8/83/Jane_Lane_by_Namelessv1.png/revision/latest/top-crop/width/200/height/150?cb=20110708183549",
+        scores: [
+            5,
+            3,
+            5,
+            1,
+            5,
+            3,
+            5,
+            2,
+            4,
+            1
+        ]
+
+    }
+
+];
+
+module.exports = friends;
