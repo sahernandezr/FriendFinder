ba93db50b1f5a7564ad1ddb015c430e4ee5091fe
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2019-12-05 18:16:57 -0600

    error in api routing corrected

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+var friends = require("../data/friends");
 
 var app = express();
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+//var friends = require("./app/data/friends");
 
 var app = express();
 const host = '0.0.0.0';
@@ -8,9 +9,11 @@ var PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname+'/app/public/')));
+app.use(express.static(path.join(__dirname+'/app/data/')));
 
 require("./app/routing/htmlRoutes.js")(app);
 require("./app/routing/apiRoutes.js")(app);
+require("./app/data/friends.js");
 
 app.listen(PORT, host, function () {
     console.log(`Our app is running on port ${PORT}`);
