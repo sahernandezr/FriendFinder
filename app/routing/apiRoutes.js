84ad564202fd629728ea369cb6b8464c78611724
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2019-12-08 21:24:19 -0600

    comparison logic added

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,17 +4,59 @@ var friends = require("../data/friends");
 
 var app = express();
 
-module.exports = function(app) {
+module.exports = function (app) {
 
     app.get("/api/friends", (req, res) => {
         return res.json(friends);
     });
-    
-    app.post("/api/friends", (req, res) => {
+
+    app.post("/api/friend", (req, res) => {
         var newFriend = req.body;
         friends.push(newFriend);
         res.json(newFriend);
+
+        var bestMatch = {
+            name: "",
+            photo: ""
+          };
+
+        var allMatches = [];
+
+        function findMatch() {
+            for (var i = 0; i < friends.length-1; i++) {
+                var matchScores = [];
+
+                for (var j = 0; j < friends[i].scores.length; j++) {
+                    matchSc = Math.abs(friends[i].scores[j] - newFriend.scores[j]);
+                    matchScores.push(matchSc)
+                };
+
+                var SMNUMBER = matchScores.reduce(myFunc);
+
+                function myFunc(total, num) {
+                    return total + num;
+                }
+
+                //console.log(matchScores);
+                //console.log(SMNUMBER);
+                allMatches.push(SMNUMBER);
+
+                
+            }
+                // console.log(allMatches);
+                // console.log("Min " + (Math.min(...allMatches)));
+                // console.log("Index of min " + (allMatches.indexOf(Math.min(...allMatches))));
+                bestMatch.name = friends[(allMatches.indexOf(Math.min(...allMatches)))].name;
+                bestMatch.photo = friends[(allMatches.indexOf(Math.min(...allMatches)))].photo;
+                console.log("Best match name: " + bestMatch.name);
+                console.log("Best match photo: "+ bestMatch.photo);
+
+        };
+
+        findMatch();
+       
+
     });
 
-
+    
 }
